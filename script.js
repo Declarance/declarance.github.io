893fea92254f6bb6987a83d54d6eeb64b9a4ecc3
Declarance <35772862+Declarance@users.noreply.github.com>
2018-05-21 16:53:41 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+﻿function changeBackgroundColor () {
+    var red = document.getElementById("red").value;
+    var blue = document.getElementById("blue").value;
+    var green = document.getElementById("green").value;
+    document.body.style.backgroundColor = 'rgb('+red+','+blue+','+green+')';
+}
