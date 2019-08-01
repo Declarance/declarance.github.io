d63f1e1f6d0a8fbebb770c0e1742937ac4e16d96
Максим Бондаренко <declarance@MacBook-Pro-Maksim.local>
2019-08-02 03:34:08 +0500

    Nice

--- js/script.js
+++
@@ -1,6 +0,0 @@
-﻿function changeBackgroundColor () {
-    var red = document.getElementById("red").value;
-    var blue = document.getElementById("blue").value;
-    var green = document.getElementById("green").value;
-    document.body.style.backgroundColor = 'rgb('+red+','+blue+','+green+')';
-}
